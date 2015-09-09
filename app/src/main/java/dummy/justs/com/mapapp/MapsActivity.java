c028b2c4f13187cbbcabde14c5cbd7a344e27c7b
justs <justs@eptron.eu>
2015-09-09 12:59:45 +0300

    -colored all the map red -first tryout of tile overlays

--- app/src/main/java/dummy/justs/com/mapapp/MapsActivity.java
+++ app/src/main/java/dummy/justs/com/mapapp/MapsActivity.java
@@ -2,6 +2,7 @@ package dummy.justs.com.mapapp;
 
 import android.app.usage.UsageEvents;
 import android.graphics.Color;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,23 +14,28 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.gms.maps.model.UrlTileProvider;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import de.greenrobot.event.EventBus;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnCameraChangeListener, View.OnClickListener {
-    private float mBoundZoom;
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener, GoogleMap.OnMarkerDragListener {
     private LatLng mSWCorner, mNECorner;
     private GoogleMap mMap;
-    private LatLng mBoundLatLng;
     private Button mButton;
 
     @Override
@@ -55,42 +61,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Nowhere"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.setOnCameraChangeListener(this);
         mMap.getUiSettings().setTiltGesturesEnabled(false);
         mMap.getUiSettings().setRotateGesturesEnabled(false);
 
-        mSWCorner = new LatLng(56, 24);
-        mNECorner = new LatLng(59, 29);
+        mMap.setOnMarkerDragListener(this);
 
-        mBoundZoom = 7;
+        mSWCorner = new LatLng(57, 27);
+        mNECorner = new LatLng(58, 28);
+
+        UrlTileProvider provider=new UrlTileProvider(512,512) {
+            @Override
+            public URL getTileUrl(int x, int y, int zoom) {
+                if (zoom==4 && (x>=5 && x<=12) && (y>=5 && x<=12)){
+                    try {
+                        return new URL("http://people.mozilla.org/~faaborg/files/shiretoko/firefoxIcon/firefox-512-noshadow.png");
+                    }
+                    catch (MalformedURLException e){
+                        e.printStackTrace();
+                        return null;
+                    }
+                }
+                else
+                return null;
+            }
+        };
+
+        TileOverlay t=mMap.addTileOverlay(new TileOverlayOptions().tileProvider(provider));
 
 
         mMap.addPolygon(new PolygonOptions()
-                .add(new LatLng(56, 24), new LatLng(56, 29), new LatLng(59, 29), new LatLng(56, 24))
+                //.add(new LatLng(56, 24), new LatLng(56, 29), new LatLng(59, 29), new LatLng(56, 24))
+                .add(new LatLng(-85, -179.9f),
+                        new LatLng(-85, -60), new LatLng(-85, 60), new LatLng(-85, 180),
+                        new LatLng(85, 180),
+                        new LatLng(85, 60), new LatLng(85, -60), new LatLng(85, -179.9f),
+                        new LatLng(-85, -179.9f)
+                        )
                 .strokeWidth(2f)
-                .strokeColor(Color.RED)
+                .strokeColor(Color.TRANSPARENT)
                 .addHole(Arrays.asList(new LatLng(57, 27), new LatLng(57, 28), new LatLng(58, 28), new LatLng(57, 27)))
-                .fillColor(Color.argb(0x40, 00, 00, 0xFF)));
+                .fillColor(Color.argb(0x40, 0xFF, 0, 0)));
 
+        mMap.addCircle(new CircleOptions()
+                .center(new LatLng(57.25f,27.75f))
+                .fillColor(Color.argb(0x80, 0, 0xFF, 0))
+                .radius(10000)
+                .strokeWidth(0));
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        EventBus.getDefault().register(this);
-        /* new Timer().scheduleAtFixedRate(new TimerTask() {
-             @Override
-             public void run() {
-                 EventBus.getDefault().post(new ResetMapEvent());
-             }
-         }, 0, 1000);*/
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        EventBus.getDefault().unregister(this);
     }
 
     private void resetMap() {
@@ -99,45 +116,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    public void onEventMainThread(ResetMapEvent event) {
-        Log.i("mapapp", "camera moved");
-        if (mMap != null)
-            resetMap();
-    }
-
-    @Override
-    public void onCameraChange(CameraPosition cameraPosition) {
-
-        if (cameraPosition.zoom < 7) {
-            mBoundZoom = 7;
-        } else if (cameraPosition.zoom > 9) {
-            mBoundZoom = 9;
-        } else
-            mBoundZoom = cameraPosition.zoom;
-
-        double lat = cameraPosition.target.latitude;
-        double lng = cameraPosition.target.longitude;
-        double maxLat = 59;
-        double minLat = 56;
-        double maxLong = 29;
-        double minLong = 24;
-
-        /*if (mBoundLat>maxLat) mBoundLat=maxLat;
-        if (mBoundLat<minLat) mBoundLat=minLat;
-        if (mBoundLong>maxLong) mBoundLong=maxLong;
-        if (mBoundLong<minLong) mBoundLong=minLong;
-*/
-
-        if (lat > mNECorner.latitude) lat = mNECorner.latitude;
-        if (lat < mSWCorner.latitude) lat = mSWCorner.latitude;
-        if (lng > mNECorner.longitude) lng = mNECorner.longitude;
-        if (lng < mNECorner.longitude) lng = mNECorner.longitude;
-
-        mBoundLatLng = new LatLng(lat, lng);
-    }
-
     @Override
     public void onClick(View v) {
         resetMap();
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        marker.setTitle(marker.getPosition().latitude+" "+marker.getPosition().longitude);
     }
 }
--- app/src/main/java/dummy/justs/com/mapapp/ResetMapEvent.java
+++
@@ -1,7 +0,0 @@
-package dummy.justs.com.mapapp;
-
-/**
- * Created by eptron on 9/7/2015.
- */
-public class ResetMapEvent {
-}
